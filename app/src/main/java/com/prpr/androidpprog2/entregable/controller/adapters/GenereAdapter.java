8d09f8bc123e169d14db7494663e2f7b1465643e
bernatsc <bernat.segura.carmona@gmail.com>
2020-04-11 22:50:28 +0200

    SearchActivity: animacio de color als generes #98

--- app/src/main/java/com/prpr/androidpprog2/entregable/controller/adapters/GenereAdapter.java
+++ app/src/main/java/com/prpr/androidpprog2/entregable/controller/adapters/GenereAdapter.java
@@ -1,14 +1,22 @@
 package com.prpr.androidpprog2.entregable.controller.adapters;
 
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Color;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.DecelerateInterpolator;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.core.graphics.ColorUtils;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.prpr.androidpprog2.entregable.R;
@@ -24,6 +32,10 @@ public class GenereAdapter extends RecyclerView.Adapter<GenereAdapter.ViewHolder
     private ArrayList<Genre> generes;
     private Context mContext;
     private GenreCallback mCallback;
+
+    private float fractionAnim;
+    ValueAnimator valueAnimator = ValueAnimator.ofFloat(0.0f, 1.0f, 0.0f);
+
 
     public GenereAdapter(Context context, final GenreCallback itemClickCallback, ArrayList<Genre> generes ) {
         this.generes = generes;
@@ -56,16 +68,37 @@ public class GenereAdapter extends RecyclerView.Adapter<GenereAdapter.ViewHolder
         return generes != null ? generes.size():0;
     }
 
-
     public class ViewHolder extends RecyclerView.ViewHolder {
 
         LinearLayout mLayout;
         TextView nomGenere;
 
+        public void setFractionAnim() {
+            fractionAnim = (float) valueAnimator.getAnimatedValue();;
+        }
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             mLayout = (LinearLayout) itemView.findViewById(R.id.genere_item_layout);
             nomGenere = (TextView) itemView.findViewById(R.id.genere_name);
+
+            //Canvi de colors de fons
+            valueAnimator.setDuration(2000);
+            valueAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+                @Override
+                public void onAnimationUpdate(ValueAnimator valueAnimator) {
+
+                    setFractionAnim();
+
+                    nomGenere.setBackgroundColor(ColorUtils.blendARGB(Color.parseColor("#15C872")
+                            , Color.parseColor("#064A28")
+                            , fractionAnim));
+                }
+            });
+            valueAnimator.setRepeatCount(Animation.INFINITE);
+            valueAnimator.start();
+
+
         }
     }
 }
